3144315244d9e1ff238089b7a8d3bf22c975c9df
Yvan Gadeau <ygadeau@sugarcrm.com>
2016-08-18 23:36:14 +0200

    Add background video
    - Change css navbar

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,21 +8,16 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <img id="ribbon-left" src="images/ribbon_left.png" />
-        <img id="ribbon-right" src="images/ribbon_right.png" />
         <div id="logos-container">
           <img id="logo-accomp" src="images/logo_accomp.jpeg" />
           <img id="logo-snam" src="images/logo_snam.png" />
         </div>
-        <div id="img-container">
-          <img id="img-benoit" src="images/benoit-cut.png" />
-          <div id="slogan">
-            <p>...et la montagne est à vous!</p>
-          </div>
-        </div>
-        <footer>
-          <Navbar  items={ ['Qui suis-je?', 'Mes randonnées', 'Photos', 'Réservations'] } />
-        </footer>
+        <video className="background-video" autoPlay loop>
+          <source src="videos/background.mp4" type="video/mp4" />
+        </video>
+        <header>
+          <Navbar  items={ ['Qui suis-je?', 'Mes randonnées', 'Galerie', 'Réservations'] } />
+        </header>
       </div>
     );
   }
